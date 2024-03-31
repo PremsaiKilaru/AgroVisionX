b5822af8ca47dcc37525e9839b2f31fd06976437
Gowthamkurella <gowtham.kurella.12@gmail.com>
2024-03-31 11:25:41 +0530

    4th push

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,jsonify
+from PIL import Image
+
 
 app = Flask(__name__)
 app = Flask(__name__, static_folder='assets')
@@ -13,6 +15,25 @@ def upload():
 @app.route('/services')
 def results():
     return render_template('services.html')
+
+
+@app.route('/upload', methods=['POST'])
+def handle_image_upload():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    print(file)
+    image = Image.open(file.stream)
+    print("seperator")
+    print(image)
+    
+    prediction = "Disease Detected: DummyResponse"
+
+    return jsonify({'prediction': prediction})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
